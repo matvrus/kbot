776f9ec832885353d65d4498d1e9a4ef1f05c572
matvrus <matviivn@ucu.edu.ua>
2023-06-16 22:51:26 +0300

    bot-telemetry

--- cmd/kbot.go
+++ cmd/kbot.go
@@ -1,16 +1,16 @@
 /*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+Copyright © 2023 NAME HERE den.vasyliev@gmail.com
 */
 package cmd
 
 import (
 	"context"
 	"fmt"
-	"log"
 	"os"
 	"time"
 
 	"github.com/spf13/cobra"
+
 	"github.com/hirosassa/zerodriver"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
@@ -21,40 +21,52 @@ import (
 )
 
 var (
-	// Telegram bot token
-	TeleToken = os.Getenv("TELE_TOKEN")
+	// TeleToken bot
+	TeleToken   = os.Getenv("TELE_TOKEN")
 	// MetricsHost exporter host:port
 	MetricsHost = os.Getenv("METRICS_HOST")
 )
 
-// Initialize OpenTelemetry for metrics
+// Initialize OpenTelemetry
 func initMetrics(ctx context.Context) {
+
 	// Create a new OTLP Metric gRPC exporter with the specified endpoint and options
-	exporter, err := otlpmetricgrpc.New(
+	exporter, _ := otlpmetricgrpc.New(
 		ctx,
 		otlpmetricgrpc.WithEndpoint(MetricsHost),
 		otlpmetricgrpc.WithInsecure(),
 	)
-	if err != nil {
-		log.Fatal(err)
-	}
 
 	// Define the resource with attributes that are common to all metrics.
-	resourceAttributes := []attribute.KeyValue{
+	// labels/tags/resources that are common to all metrics.
+	resource := resource.NewWithAttributes(
 		semconv.SchemaURL,
 		semconv.ServiceNameKey.String(fmt.Sprintf("kbot_%s", appVersion)),
-	}
-
-	resource := resource.NewWithAttributes(resourceAttributes...)
-
-	// Create a new MeterProvider with the specified resource and exporter
-	provider := sdkmetric.NewMeterProvider(
-		sdkmetric.WithResource(resource),
-		sdkmetric.WithBatcher(exporter),
 	)
 
-	// Register the provider globally
-	otel.SetMeterProvider(provider)
+	// Create a new MeterProvider with the specified resource and reader
+	mp := sdkmetric.NewMeterProvider(
+		sdkmetric.WithResource(resource),
+		sdkmetric.WithReader(
+			// collects and exports metric data every 10 seconds.
+			sdkmetric.NewPeriodicReader(exporter, sdkmetric.WithInterval(10*time.Second)),
+		),
+	)
+
+	// Set the global MeterProvider to the newly created MeterProvider
+	otel.SetMeterProvider(mp)
+
+}
+
+func pmetrics(ctx context.Context, payload string) {
+	// Get the global MeterProvider and create a new Meter with the name "kbot_light_signal_counter"
+	meter := otel.GetMeterProvider().Meter("kbot_light_signal_counter")
+
+	// Get or create an Int64Counter instrument with the name "kbot_light_signal_<payload>"
+	counter, _ := meter.Int64Counter(fmt.Sprintf("kbot_light_signal_%s", payload))
+
+	// Add a value of 1 to the Int64Counter
+	counter.Add(ctx, 1)
 }
 
 // kbotCmd represents the kbot command
@@ -69,10 +81,8 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		ctx := context.Background()
-		initMetrics(ctx)
+		logger := zerodriver.NewProductionLogger()
 
-		fmt.Println("kbot %s started", appVersion)
 		kbot, err := telebot.NewBot(telebot.Settings{
 			URL:    "",
 			Token:  TeleToken,
@@ -80,20 +90,42 @@ to quickly create a Cobra application.`,
 		})
 
 		if err != nil {
-			log.Fatalf("Please check TELE_TOKEN env variable. %s", err)
+			logger.Fatal().Str("Error", err.Error()).Msg("Please check TELE_TOKEN")
 			return
+		} else {
+			logger.Info().Str("Version", appVersion).Msg("kbot started")
+
 		}
 
 		kbot.Handle(telebot.OnText, func(m telebot.Context) error {
-			log.Print(m.Message().Payload, m.Text())
+			logger.Info().Str("Payload", m.Text()).Msg(m.Message().Payload)
+
 			payload := m.Message().Payload
+			pmetrics(context.Background(), payload)
 
 			switch payload {
 			case "hello":
-				err = m.Send(fmt.Sprintf("Hello, %s! I'm Kbot %s!", m.Sender().FirstName, appVersion))
+				err = m.Send(fmt.Sprintf("Hello I'm Kbot %s!", appVersion))
+			case "help":
+				err = m.Send("I can help you with the following commands:\n\n"+
+					"/hello - I will say hello to you\n"+
+					"/help - I will show you this help message\n"+
+					"/version - I will show you my version\n"+
+					"/metrics - I will show you my metrics\n"+
+					"/stop - I will stop myself\n")
+			case "version":
+				err = m.Send(fmt.Sprintf("I'm Kbot %s!", appVersion))
+			case "metrics":
+				err = m.Send(fmt.Sprintf("I'm Kbot %s!", appVersion))
+			case "stop":
+				err = m.Send(fmt.Sprintf("I'm Kbot %s!", appVersion))
+			default:
+				err = m.Send(fmt.Sprintf("I don't know what to do with %s!", payload))
+				
 			}
 
 			return err
+
 		})
 
 		kbot.Start()
@@ -114,4 +146,7 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// kbotCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	// Initialize OpenTelemetry tracer
+
 }
